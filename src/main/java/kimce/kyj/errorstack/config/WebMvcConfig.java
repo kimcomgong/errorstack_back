571138f42ff1ac5a85338f8c9cde3824457452e8
kimmulgae <kimmulgae11@gmail.com>
2021-05-26 12:27:46 +0900

    WebMvc

--- src/main/java/kimce/kyj/errorstack/config/WebMvcConfig.java
+++ src/main/java/kimce/kyj/errorstack/config/WebMvcConfig.java
@@ -1,10 +1,10 @@
 package kimce.kyj.errorstack.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configurable
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private final long MAX_AGE_SECS = 3600;
 
